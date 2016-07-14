18de127a9d081e7928839796d62f215c2470a955
carlos <carlos.mataguzman@ucr.ac.cr>
2016-07-14 15:21:13 -0600

    Seleccion de objetos por capa

--- iden_geo.py
+++ iden_geo.py
@@ -21,10 +21,34 @@ def load_layer (path, name, data_source, color):
 
 #FunciÃ³n que elimna todas las capas del mapa
 def clear_all_layers():
-    layers = iface.legendInterface().layers()    
     for layer in layers:
-        QgsMapLayerRegistry.instance().removeMapLayer(layer.id())
+        QgsMapLayerRegistry.instance().removeMapLayer(layers[layer].id())
     return 
+
+#Función que selecciona elementos de una capa
+#feature attributes es un diccionario
+#layer es una capa del mapa
+def get_features(layer, feature_attribute):
+    selection = []
+    for feature in layer.getFeatures():
+        value = feature.attributes[1]
+        if value == feature.attribute(feature_attribute[0]):
+           selection.append(feature.id())
+           print feature.name()
+    return selection
+
+#Función que selecciona elementos de una capa
+#layer es una capa del mapa
+#feature_attribute es una tupla, su primer valor es el atributo y el segundo es el valor buscado
+#para crear una tupla nombre = (valor1, valor2)
+def get_features(layer, feature_attribute):
+    selection = []
+    for feature in layer.getFeatures():
+        value = feature.attribute(feature_attribute[0])
+        if value == feature_attribute[1]:
+           selection.append(feature.id())
+    layer.setSelectedFeatures(selection)       
+    return selection
 
 
 # Se carga la capa de PROPIEDADES_RodrigoFacio del mapa
@@ -45,15 +69,15 @@ parqueos_layer = load_layer("Shapefiles_RodrigoFacio/PARQUEOS_RodrigoFacio.shp",
 # Se carga la capa de CALLES del mapa ------ tener en consideraciÃ³n la ruta del archivo
 calles_layer = load_layer("Shapefiles_RodrigoFacio/CALLES_RodrigoFacio.shp", "Calles", "ogr", "#065ea1")
 
-#hola
+
 
 
 continue_execution = True
 
-while continue_execution:    
-    response = PyQt4.QtGui.QInputDialog.getText(None, "Salir", "Presione ESC para salir.")
-    continue_execution = response[1]
+#while continue_execution:    
+#    response = PyQt4.QtGui.QInputDialog.getText(None, "Salir", "Presione ESC para salir.")
+#    continue_execution = response[1]
 
 # Se eliminan todas las capas 
-clear_all_layers()
+#clear_all_layers()
 print "Bye bye!"
