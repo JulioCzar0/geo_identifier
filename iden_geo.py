a8c8c88ccdcf9fdb3e6a9f2bc8a1e902bf25bf9a
Julio C. Guzman B <cesarguzma@gmail.com>
2016-07-13 23:12:53 -0600

    Cambio de rutas relativas

--- iden_geo.py
+++ iden_geo.py
@@ -2,38 +2,41 @@
 # -*- coding: latin-1 -*-
 import os, sys
 
+# Se debe cambiar la ruta y colocar la dirección en donde se encuentran los archivos. Ejem: 'home\paradigmas\files'
+os.chdir('C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\geo_identifier\Shapefiles_RodrigoFacio')
+
 # Se carga la capa de PROPIEDADES_RodrigoFacio del mapa ------ tener en consideración la ruta del archivo
-property_layer = QgsVectorLayer("C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\Shapefiles_RodrigoFacio\Shapefiles_RodrigoFacio\PROPIEDADES_RodrigoFacio.shp", "Fincas", "ogr")
+property_layer = QgsVectorLayer("..\Shapefiles_RodrigoFacio\PROPIEDADES_RodrigoFacio.shp", "Fincas", "ogr")
 idFincas = property_layer.id()
 # Se agrega la capa al mapa para manejarla y desplegarla en la GUI de QGGIS
 QgsMapLayerRegistry.instance().addMapLayer(property_layer)
 
 # Se carga la capa de ACERAS_RodrigoFacio del mapa ------ tener en consideración la ruta del archivo
-property_layer = QgsVectorLayer("C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\Shapefiles_RodrigoFacio\Shapefiles_RodrigoFacio\ACERAS_RodrigoFacio.shp", "Aceras", "ogr")
+property_layer = QgsVectorLayer("..\Shapefiles_RodrigoFacio\ACERAS_RodrigoFacio.shp", "Aceras", "ogr")
 idAceras = property_layer.id()
 # Se agrega la capa al mapa para manejarla y desplegarla en la GUI de QGGIS
 QgsMapLayerRegistry.instance().addMapLayer(property_layer)
 
 # Se carga la capa de OTROS_RodrigoFacio del mapa ------ tener en consideración la ruta del archivo
-property_layer = QgsVectorLayer("C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\Shapefiles_RodrigoFacio\Shapefiles_RodrigoFacio\OTROS_RodrigoFacio.shp", "Otros", "ogr")
+property_layer = QgsVectorLayer("..\Shapefiles_RodrigoFacio\OTROS_RodrigoFacio.shp", "Otros", "ogr")
 idOtros = property_layer.id()
 # Se agrega la capa al mapa para manejarla y desplegarla en la GUI de QGGIS
 QgsMapLayerRegistry.instance().addMapLayer(property_layer)
 
 # Se carga la capa EDIFICIOS_RodrigoFacio PROPIEDADES del mapa ------ tener en consideración la ruta del archivo
-property_layer = QgsVectorLayer("C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\Shapefiles_RodrigoFacio\Shapefiles_RodrigoFacio\EDIFICIOS_RodrigoFacio.shp", "Edificios", "ogr")
+property_layer = QgsVectorLayer("..\Shapefiles_RodrigoFacio\EDIFICIOS_RodrigoFacio.shp", "Edificios", "ogr")
 idEdificios = property_layer.id()
 # Se agrega la capa al mapa para manejarla y desplegarla en la GUI de QGGIS
 QgsMapLayerRegistry.instance().addMapLayer(property_layer)
 
 # Se carga la capa de PARQUEOS_RodrigoFacio del mapa ------ tener en consideración la ruta del archivo
-property_layer = QgsVectorLayer("C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\Shapefiles_RodrigoFacio\Shapefiles_RodrigoFacio\PARQUEOS_RodrigoFacio.shp", "Parqueos", "ogr")
+property_layer = QgsVectorLayer("..\Shapefiles_RodrigoFacio\PARQUEOS_RodrigoFacio.shp", "Parqueos", "ogr")
 idParqueos = property_layer.id()
 # Se agrega la capa al mapa para manejarla y desplegarla en la GUI de QGGIS
 QgsMapLayerRegistry.instance().addMapLayer(property_layer)
 
 # Se carga la capa de CALLES del mapa ------ tener en consideración la ruta del archivo
-property_layer = QgsVectorLayer("C:\Users\JulioCzar\Documents\UCR\Paradigmas\Proyecto\Shapefiles_RodrigoFacio\Shapefiles_RodrigoFacio\CALLES_RodrigoFacio.shp", "Calles", "ogr")
+property_layer = QgsVectorLayer("..\Shapefiles_RodrigoFacio\CALLES_RodrigoFacio.shp", "Calles", "ogr")
 idCalles = property_layer.id()
 # Se agrega la capa al mapa para manejarla y desplegarla en la GUI de QGGIS
 QgsMapLayerRegistry.instance().addMapLayer(property_layer)
@@ -41,7 +44,7 @@ QgsMapLayerRegistry.instance().addMapLayer(property_layer)
 os.system('read -s -n 1 -p "Press any key to continue..."')
 print
 
-
+'''
 # Se eliminan todas las capas 
 QgsMapLayerRegistry.instance().removeMapLayer(idFincas)
 QgsMapLayerRegistry.instance().removeMapLayer(idAceras)
@@ -49,3 +52,4 @@ QgsMapLayerRegistry.instance().removeMapLayer(idOtros)
 QgsMapLayerRegistry.instance().removeMapLayer(idEdificios)
 QgsMapLayerRegistry.instance().removeMapLayer(idParqueos)
 QgsMapLayerRegistry.instance().removeMapLayer(idCalles)
+'''
